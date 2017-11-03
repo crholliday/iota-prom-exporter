7507e1fae3b56656cc02259cedd732e9d2974127
chris.holliday@gmail.com <chris.holliday@gmail.com>
2017-11-03 18:41:54 -0500

    first commit for production

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 let promclient = require('prom-client')
 let Gauge = promclient.Gauge
-let Histogram = promclient.Histogram
+// let Histogram = promclient.Histogram
 
 /* 
 TODO:
@@ -18,54 +18,47 @@ TODO:
 let express = require('express')
 let app = express()
 
-let totalStats = require ('../totalstats')
-let workerStats = require ('../workerstats')
-let rate = require('../paystats')
-let rates = require('../rate')
+let nodeInfo = require ('./nodeInfo')
+let neighborInfo = require ('./neighborInfo')
 
-let totalBTC = new Gauge('overwatch_total_btc', 'Your total count of bitcoin', [ 'btc' ])
-let totalUSD = new Gauge('overwatch_total_usd', 'Your total funds in USD', [ 'usd' ])
-let totalSols = new Gauge('overwatch_total_sols', 'Your total sols/s', [ 'sols' ])
-let equihashPayrate = new Gauge('overwatch_current_payrate', 'Equihash payrate', [ 'btc' ])
-let remote_query = new Histogram('overwatch_remote_query', 'Remote queries to Nicehash', [ 'duration' ])
-let zecPrice = new Gauge('overwatch_zec_btc_price', 'Price of ZEC in BTC', ['btc'])
-
-let addr = process.env.addr
-let algo = 24
+let totalTransactions = new Gauge({ name: 'total_transactions_queued', help: 'Total open txs at the interval' })
+let totalTips = new Gauge({ name: 'total_tips', help: 'Total tips at the interval' })
+let totalNeighbors = new Gauge({ name: 'total_neighbors', help: 'Total neighbors at the interval' })
+let latestMilestone = new Gauge({ name: 'latest_milestone', help: 'Tangle milestone at the interval' })
+let latestSolidSubtangleMilestone = new Gauge({ name: 'latest_subtangle_milestone', help: 'Subtangle milestone at the interval' })
+let newTransactions = new Gauge({ name: 'new_neighbor_transactions', help: 'New transactions by neighbor', labelNames: ['id'] })
+let randomTransactions = new Gauge({ name: 'random_neighbor_transactions', help: 'Random transactions by neighbor', labelNames: ['id'] })
+let allTransactions = new Gauge({ name: 'all_neighbor_transactions', help: 'All transactions by neighbor', labelNames: ['id'] })
+let invalidTransactions = new Gauge({ name: 'invalid_neighbor_transactions', help: 'Invalid transactions by neighbor', labelNames: ['id'] })
 
 app.get('/metrics', (req, res) => {
     console.log('.')
-    let start = new Date().getTime()
-    totalStats(addr, (results) => {
-        workerStats(addr, algo, (workerResults) => {
-            try {
-                totalSols.set( workerResults.totalSols )
-                results.forEach( (r) => {
 
-                    totalBTC.set(Number(r.totalBTC))
-                    totalUSD.set(Number(r.currency['USD']))       
-                })
+    nodeInfo( (err, results) => {
 
-            } catch (e) {
-                console.error('workerStats: Bad response from remote endpoint.')
-                console.error(e)
-            }
+        if (err) {
+            console.log('there was an error in the nodeInfo api call... ')
+            console.log(err)
+        } else {
+            totalTransactions.set(results.transactionsToRequest)
+            totalTips.set(results.tips)
+            totalNeighbors.set(results.neighbors)
+            latestMilestone.set(results.latestMilestoneIndex)
+            latestSolidSubtangleMilestone.set(results.latestSolidSubtangleMilestoneIndex)           
+        }
 
-            let end = new Date().getTime()
-            remote_query.labels('duration').observe((end-start) / 1000)
-
-            rate(algo, (err, price)=> {
-
-                equihashPayrate.set(Number(price))
-                rates.getZecRate((err,zecRate)=> {
-                    zecPrice.set(Number(zecRate.rate))
-                    res.end(promclient.register.metrics())
-                })
+        neighborInfo( (error, neighborResults) => {
+            neighborResults.forEach( (r) => {              
+                newTransactions.set({id: r.address}, r.numberOfNewTransactions)
+                invalidTransactions.set({id: r.address}, r.numberOfInvalidTransactions)                       
+                allTransactions.set({id: r.address}, r.numberOfAllTransactions)                       
+                randomTransactions.set({id: r.address}, r.numberOfRandomTransactionRequests)                       
             })
-        })
+            res.end(promclient.register.metrics())      
+        })         
     })
 })
 
-app.listen(process.env.port || 9010, () => {
-    console.log('Listening')
+app.listen(process.env.port || 9011, () => {
+    console.log('Listening on:', process.env.port || 9010)
 })
---
+++ neighborInfo.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let IOTA = require('iota.lib.js')
+let config = require('./config')
+const iota = new IOTA({'provider': config.iota_node_url})
+
+let neighbhorInfo = (done) => {
+
+    let results = {}
+
+    iota.api.getNeighbors(function (error, success) {
+        if (error) {
+            console.log('error in getNodeInfo api call... ')
+            console.log(error)
+        } else {
+            results = success
+        }
+        done(error, results)
+    })   
+}
+
+module.exports = neighbhorInfo
