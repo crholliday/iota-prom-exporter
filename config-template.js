3729511d193685048a6268cecdd696b29807683c
chris.holliday@gmail.com <chris.holliday@gmail.com>
2017-12-11 15:19:14 -0600

    added config changes

--- config-template.js
+++ config-template.js
@@ -2,6 +2,13 @@ let path = require('path')
 global.rootPath = path.normalize(path.join(__dirname, '..', '..'))
 
 module.exports = {
+    // url and port of your IRI node
     iota_node_url: 'http://localhost:14700',
-    stresstest_table_url: 'http://analytics.iotaledger.net/stresstest.table'
+
+    // keep this
+    stresstest_table_url: 'http://analytics.iotaledger.net/stresstest.table',
+
+    // address and port where the exporter will be bound
+    bind_address: '127.0.0.1',
+    bind_port: 9311
 }
